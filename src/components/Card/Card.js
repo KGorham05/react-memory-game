85f35561b836f0eea2c555e1d61132c8637e2b4b
Kgor <kevin.gorham@gmail.com>
2019-11-11 16:20:32 -0800

    pokemon names displaying to page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,11 @@ import React from "react";
 function Card(props) {
     return (
         <div
-            onClick={() => props.handleClick(props.id)}
+            // onClick={() => props.handleClick(props.id)}
             style={{ backgroundImage: `url("${props.image}")` }}
-        />
+        >
+            {props.img}
+        </div>
     )
 }
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "../Nav";
 import Header from "../Header";
 import Footer from "../Footer";
+import Card from "../Card";
 import pokeArr from "../../pokemon.json";
 import "./style.css";
 
@@ -11,38 +12,51 @@ class Game extends Component {
         pokeInPlay: []
     };
 
-    getRandomPokemon = (list, quantity) => {
+    getRandomPokemon = (list, n) => {
         const pokemonSelected = [];
-        for (let i = 0; i < quantity; i++) {
-            let randomIndex = Math.floor(Math.random() * list.length)
+        // loop through n times 
+        for (let i = 0; i < n; i++) {
+            let randomIndex = Math.floor(Math.random() * list.length);
             let pick = list[randomIndex];
             pokemonSelected.push(pick);
+            // remove item from array to prevent duplicates
             list.splice(randomIndex, 1);
         }
-        this.setState({pokeInPlay: pokemonSelected}, () => {
+        this.setState({ pokeInPlay: pokemonSelected }, () => {
             console.log(this.state.pokeInPlay);
         });
-        
     }
 
     componentDidMount() {
         this.getRandomPokemon(pokeArr, 10);
     }
     // handleClick = () => {
-        
+
     // }
 
     render() {
-        return (
-            <div>
-                <Nav />
-                <Header />
-                <div className="game-board">
-                    {/* Map over array of cards */}
+        if (this.state.pokeInPlay.length === 0) {
+            return (
+                <div>
+                    Randomly Selecting your Pokemon!
                 </div>
-                <Footer />
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div>
+                    <Nav />
+                    <Header />
+                    <div className="game-board">
+                        {/* Map over array of cards */}
+                        Pokemon have been selected!
+                        {this.state.pokeInPlay.map(poke => (
+                            <Card img={poke} />
+                        ))}
+                    </div>
+                    <Footer />
+                </div>
+            )
+        }
     }
 }
 
