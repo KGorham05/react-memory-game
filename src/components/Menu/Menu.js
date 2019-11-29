c87762564a1f30512dc08ff720a9b9c03966ea9c
Kgor <kevin.gorham@gmail.com>
2019-11-29 15:26:44 -0800

    updated menu styles

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,28 +7,21 @@ function Menu(props) {
     return (
         <div id="overlay" className="container-fluid">
             <div className="row">
-                <div className="col-md-4 offset-md-4 menu ">
+                <div className="col-md-6 offset-md-3 menu ">
 
                     <div className="row">
                         <div className="col-md-12">
                             <h1 className="menu-title text-center">PoKeMoN MeMoRy!</h1>
                         </div>
                     </div>
+
                     <div className="row">
-                        <div className="col-md-12">
-                            <ul className="directions">
-                                <li>Click on the cards!</li>
-                                <li>Match all the cards to win!</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4 offset-md-8">
+                        <div className="col-md-12 text-center">
                             <button className="start-btn btn btn-primary" onClick={props.handleClick}>Start Game!</button>
                         </div>
                     </div>
 
-
+        
 
                 </div>
             </div>
